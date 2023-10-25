6b7f3caa59a2290e401b47a2c25953637d2cd285
Deyvi Condori <deivcondor@gmail.com>
2023-10-24 23:30:57 -0500

    Agregar archivos de proyecto.

---
+++ Labo 10- biblio/Biblioteca/Producto.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Producto
+{
+    public string Nombre { get; set; }
+    public string NombreReporte { get; set; }
+    public int Ventas { get; set; }
+
+    public Producto(string nombre, string nombreReporte)
+    {
+        Nombre = nombre;
+        NombreReporte = nombreReporte;
+        Ventas = 0;
+    }
+
+    public void RegistrarVenta()
+    {
+        Console.Clear();
+        Console.WriteLine($"================================\nRegistrar venta de:\n================================");
+        Console.WriteLine($"Se va a registrar la venta de un {Nombre} ¿Desea Continuar?");
+        Console.WriteLine("1: Sí\n2: No\n================================");
+        int opcion = PedirOpcion();
+
+        if (opcion == 1)
+        {
+            Console.Write($"Ingrese unidades vendidas de {Nombre}: ");
+            int ventas = int.Parse(Console.ReadLine());
+            Ventas += ventas;
+            Console.WriteLine($"Se han registrado {ventas} ventas para {Nombre}.");
+        }
+        Console.WriteLine("\nPresione una tecla para continuar...");
+        Console.ReadKey();
+    }
+
+    public void RegistrarDevolucion()
+    {
+        Console.Clear();
+        Console.WriteLine($"================================\nRegistrar devolución de:\n================================");
+        Console.WriteLine($"Se va a registrar la devolución de {Nombre} ¿Desea Continuar?");
+        Console.WriteLine("1: Sí\n2: No\n================================");
+        int opcion = PedirOpcion();
+
+        if (opcion == 1 && Ventas > 0)
+        {
+            Console.Clear();
+            Console.Write($"¿Cuántas unidades de {Nombre} desea devolver? \n (Actualmente ha vendido {Ventas} unidades): ");
+            int devoluciones = int.Parse(Console.ReadLine());
+
+            if (devoluciones <= Ventas)
+            {
+                Ventas -= devoluciones;
+                Console.WriteLine($"Se ha registrado la devolución de {devoluciones} unidades de {Nombre}.");
+            }
+            else
+            {
+                Console.WriteLine("No puede devolver más unidades de las que ha vendido.");
+            }
+        }
+        else if (Ventas <= 0)
+        {
+            Console.WriteLine($"No hay ventas registradas de {Nombre} para devolver.");
+        }
+
+        Console.WriteLine("\nPresione una tecla para continuar...");
+        Console.ReadKey();
+    }
+
+    private int PedirOpcion()
+    {
+        Console.Write("Ingrese una opción: ");
+        return int.Parse(Console.ReadLine());
+    }
+}
+
---
+++ Labo 10- biblio/Biblioteca/tienda.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Tienda
+{
+    public List<Producto> Productos { get; set; }
+
+    public Tienda()
+    {
+        Productos = new List<Producto>
+        {
+            new Producto("mPhone 3000", "mPhones"),
+            new Producto("mPad 3500", "mPads"),
+            new Producto("MAPBrook 3800", "MAPBrooks"),
+            new Producto("mWatch 8000", "mWatches")
+        };
+    }
+    public void MostrarMenu()
+    {
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine("================================\nProductos de mPhone\n================================");
+            for (int i = 0; i < Productos.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}: Ventas de {Productos[i].Nombre}");
+            }
+            Console.WriteLine($"{Productos.Count + 1}: Salir\n================================");
+            int opcion = PedirOpcion();
+
+            if (opcion >= 1 && opcion <= Productos.Count)
+            {
+                var producto = Productos[opcion - 1];
+                Console.Clear();
+                Console.WriteLine($"================================\nRegistrar Venta de {producto.Nombre}\n================================");
+                Console.WriteLine("1: Registrar Venta\n2: Registrar Devolución\n3: Menu Principal\n================================");
+                int subopcion = PedirOpcion();
+
+                if (subopcion == 1)
+                {
+                    producto.RegistrarVenta();
+                }
+                else if (subopcion == 2)
+                {
+                    producto.RegistrarDevolucion();
+                }
+            }
+            else if (opcion == Productos.Count + 1)
+            {
+                MostrarEstadisticasFinales();
+                return;
+            }
+            else
+            {
+                Console.WriteLine("Opción inválida. Intente nuevamente.");
+            }
+        }
+    }
+
+    public void MostrarEstadisticasFinales()
+    {
+        int totalVendido = 0;
+
+        Console.Clear();
+        Console.WriteLine("================================\nReporte Final\n================================");
+        Console.WriteLine("Productos Vendidos | Cantidad");
+        Console.WriteLine("--------------------------------");
+
+        foreach (var producto in Productos)
+        {
+            Console.WriteLine($"{producto.NombreReporte.PadRight(20)}| {producto.Ventas}");
+            totalVendido += producto.Ventas;
+        }
+
+        Console.WriteLine("--------------------------------");
+        Console.WriteLine($"Total{"".PadRight(15)}| {totalVendido}");
+        Console.WriteLine("================================");
+        Console.WriteLine("¡Hasta Luego!\n\n\n\nDiseñado Por: [Deyvi Condori]");
+
+        Console.ReadLine();
+    }
+    private int PedirOpcion()
+    {
+        Console.Write("Ingrese una opción: ");
+        return int.Parse(Console.ReadLine());
+    }
+}
